d1fc1999f23ebfe86adc2f28b1f002728c014d19
terrell-greene <33211911+terrell-greene@users.noreply.github.com>
2019-12-11 11:53:01 -0600

    Map now starts out at user location, and shows court markers

--- screens/map/map.screen.tsx
+++ screens/map/map.screen.tsx
@@ -1,10 +1,53 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { StyleSheet, View, Platform } from 'react-native'
 import { NavigationStackScreenComponent } from 'react-navigation-stack'
-import MapView from 'react-native-maps'
+import MapView, { Marker } from 'react-native-maps'
+
+import { GraphQL } from '../../apollo'
+import { useQuery } from '@apollo/react-hooks'
+import { Court } from '../../apollo/graphql/types.graphql'
+
+const { Client } = GraphQL
 
 const Map: NavigationStackScreenComponent = () => {
-  return <MapView style={StyleSheet.absoluteFillObject}></MapView>
+  const map = useRef<MapView>(null)
+
+  const {
+    data: { mapRegion }
+  } = useQuery(Client.Query.GetMapRegion)
+  const {
+    data: { courts }
+  } = useQuery<{ courts: Court[] }>(Client.Query.GetCourts)
+
+  const buildMarker = (court: Court) => {
+    // const routeParams: CourtSessionsRouteParams = {
+    //   courtId: court.id,
+    //   courtName: court.name
+    // }
+
+    return (
+      <Marker
+        key={court.id}
+        title={court.name}
+        coordinate={{
+          latitude: court.coords.latitude,
+          longitude: court.coords.longitude
+        }}
+        // onCalloutPress={() => navigate('CourtSessions', routeParams)}
+      />
+    )
+  }
+
+  return (
+    <MapView
+      style={StyleSheet.absoluteFillObject}
+      ref={map}
+      initialRegion={mapRegion}
+      showsUserLocation
+    >
+      {courts.map(court => buildMarker(court))}
+    </MapView>
+  )
 }
 
 export default Map
