a4b4de4012fbbed1008c05761cc85aa992dc50e0
terrell-greene <33211911+terrell-greene@users.noreply.github.com>
2019-12-11 10:07:27 -0600

    Started getting default data and permissions before app can be used

--- App.tsx
+++ App.tsx
@@ -1,17 +1,45 @@
 import React, { useState } from 'react'
-import { AppLoading } from 'expo'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import { ApolloProvider } from '@apollo/react-hooks'
 import { AppearanceProvider } from 'react-native-appearance'
+import { AppLoading } from 'expo'
+import { getItemAsync } from 'expo-secure-store'
+import * as Permissions from 'expo-permissions'
+import * as Location from 'expo-location'
 
 import { client } from './apollo'
-import { defaultViewStyle } from './constants'
 import Navigator from './navigation'
 
 export default function App() {
   const [isReady, setIsReady] = useState(false)
 
-  const appLoad = async () => {}
+  const appLoad = async () => {
+    await getPersistedUser()
+    await getLocation()
+    await fetchCourtAndSessions()
+  }
+
+  const getPersistedUser = async () => {
+    const auth = await getItemAsync('auth')
+
+    console.log(auth)
+  }
+
+  const getLocation = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION)
+
+    if (status !== 'granted') {
+      return
+    }
+
+    let {
+      coords: { latitude, longitude }
+    } = await Location.getCurrentPositionAsync()
+
+    console.log(latitude, longitude)
+  }
+
+  const fetchCourtAndSessions = async () => {}
 
   return isReady ? (
     <AppearanceProvider>
