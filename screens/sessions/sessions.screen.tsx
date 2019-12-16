1f4bc11d18e71282328e190bfa7184e679f9e597
terrell-greene <33211911+terrell-greene@users.noreply.github.com>
2019-12-15 22:20:09 -0600

    Added functionality to session screen

--- screens/sessions/sessions.screen.tsx
+++ screens/sessions/sessions.screen.tsx
@@ -1,20 +1,37 @@
 import React from 'react'
 import { NavigationStackScreenComponent } from 'react-navigation-stack'
-import { View, Text } from 'react-native'
-import { colorGrey } from '../../constants'
+import { FlatList } from 'react-native'
+import { useQuery } from '@apollo/react-hooks'
+
+import { Query } from '../../apollo'
+import { Session } from '../../apollo/graphql/types.graphql'
+import { keyExtractor } from '../../utils'
+import SessionListItem from '../../components/session-list-item/session-list-item.component'
+import ScheduleSessionLink from '../../components/schedule-session-link/schedule-session-link.component'
 
 const SessionsScreen: NavigationStackScreenComponent = () => {
+  const {
+    data: { sessions }
+  } = useQuery<{ sessions: Session[] }>(Query.GetSessions)
+
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-      }}
-    >
-      <Text style={{ color: colorGrey }}>Hello, World!</Text>
-    </View>
+    <React.Fragment>
+      {sessions.length > 0 ? (
+        <FlatList
+          keyExtractor={keyExtractor}
+          data={sessions}
+          renderItem={({ item }) => <SessionListItem session={item} />}
+        />
+      ) : (
+        <ScheduleSessionLink />
+      )}
+    </React.Fragment>
   )
+}
+
+SessionsScreen.navigationOptions = {
+  title: 'Hoop Sessions',
+  headerRight: <ScheduleSessionLink btn />
 }
 
 export default SessionsScreen
