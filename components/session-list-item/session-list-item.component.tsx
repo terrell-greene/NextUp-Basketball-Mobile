f17f37268fa9e25132f53b75563c1f84cee1aa6a
terrell-greene <33211911+terrell-greene@users.noreply.github.com>
2019-12-17 21:19:33 -0600

    added route session screen to session stack

--- components/session-list-item/session-list-item.component.tsx
+++ components/session-list-item/session-list-item.component.tsx
@@ -1,19 +1,27 @@
 import React from 'react'
+import { useNavigation } from 'react-navigation-hooks'
 
 import { Session } from '../../apollo/graphql/types.graphql'
 import { ListItemContainer, ListItemText } from './session-list-item.styles'
 import SessionIcons from '../session-icons/session-icons.component'
+import { SessionDetailsRouteParams } from '../../screens/session-details/session-details.screen'
 
 interface SessionListItemProps {
   session: Session
 }
 
 const SessionListItem: React.FC<SessionListItemProps> = ({ session }) => {
+  const { navigate } = useNavigation()
   const { id, court, date, times, numberAttending } = session
+
+  const routeParams: SessionDetailsRouteParams = {
+    sessionId: id
+  }
+
   return (
     <ListItemContainer
       title={court.name}
-      onPress={() => console.log('go to details')}
+      onPress={() => navigate('SessionDetails', routeParams)}
       subtitle={
         <React.Fragment>
           <ListItemText>{`${date}: ${times}`}</ListItemText>
--- navigation/sessions.stack.ts
+++ navigation/sessions.stack.ts
@@ -3,11 +3,13 @@ import { createStackNavigator } from 'react-navigation-stack'
 import { defaultStackNavigationOptions, defaultViewStyle } from '../constants'
 import Sessions from '../screens/sessions/sessions.screen'
 import CreateEditSession from '../screens/create-edit-session/create-edit-session.screen'
+import SessionDetails from '../screens/session-details/session-details.screen'
 
 const SessionsStack = createStackNavigator(
   {
     Sessions,
-    CreateEditSession
+    CreateEditSession,
+    SessionDetails
   },
   {
     cardStyle: defaultViewStyle,
