2b752e93a702d90d46865ea48a3c399681f1cf3f
terrell-greene <33211911+terrell-greene@users.noreply.github.com>
2019-12-18 20:13:54 -0600

    added isEmpty utility function

--- utils.ts
+++ utils.ts
@@ -1,1 +1,6 @@
 export const keyExtractor = item => item.id.toString()
+
+export const isEmpty = (value: any) =>
+  value === undefined ||
+  value === null ||
+  (typeof value === 'object' && Object.keys(value).length === 0)
